fc28d7f31116e8ef8b582f64e2c955b5999d4fe8
Jörg Battermann <jb@joergbattermann.com>
2022-10-14 14:23:23 -0400

    Removing standard footer now

--- src/index.ts
+++ src/index.ts
@@ -20,37 +20,54 @@ export default {
 		request: Request,
 		env: Env,
 		ctx: ExecutionContext): Promise<Response> {
-			
-			const { pathname }  = new URL(request.url)
-			const { searchParams } = new URL(request.url)
-			
-			console.log("Received request for '" + request.url + "' with an extracted 'pathname' value of '" + pathname + "' and a 'searchParams' value of '" + searchParams.toString() +"'");
 
-			let reverseProxiedRequestUrlString = ''
+		const { pathname } = new URL(request.url)
+		const { searchParams } = new URL(request.url)
 
-			if(!(!!pathname) || pathname === '/') {
-				reverseProxiedRequestUrlString = standardResumeUrl
-			}
-			else if (pathname === '/icons/favicon.ico'){
-				reverseProxiedRequestUrlString = faviconUrl
-			}
-			else if (pathname.startsWith('/icons/favicon')){
-				return new Response(null, { status: 404, })
-			}
-			else {
-				reverseProxiedRequestUrlString = standardResumeBaseUrl + pathname
-			}
+		console.log("Received request for '" + request.url + "' with an extracted 'pathname' value of '" + pathname + "' and a 'searchParams' value of '" + searchParams.toString() + "'");
 
-			const reverseProxiedRequestUrl = new URL(reverseProxiedRequestUrlString)
-			
-			if(!!searchParams){
-				for (let searchParam of searchParams) {
-					reverseProxiedRequestUrl.searchParams.set(searchParam[0], searchParam[1])
-				}
+		let reverseProxiedRequestUrlString = ''
+		let isMainResumeUrl = false
+
+		if (!(!!pathname) || pathname === '/') {
+			reverseProxiedRequestUrlString = standardResumeUrl
+			isMainResumeUrl = true
+		}
+		else if (pathname === '/icons/favicon.ico') {
+			reverseProxiedRequestUrlString = faviconUrl
+		}
+		else if (pathname.startsWith('/icons/favicon')) {
+			return new Response(null, { status: 404, })
+		}
+		else {
+			reverseProxiedRequestUrlString = standardResumeBaseUrl + pathname
+		}
+
+		const reverseProxiedRequestUrl = new URL(reverseProxiedRequestUrlString)
+
+		if (!!searchParams) {
+			for (let searchParam of searchParams) {
+				reverseProxiedRequestUrl.searchParams.set(searchParam[0], searchParam[1])
 			}
+		}
 
-			console.log("Fetching and returning data from '" + reverseProxiedRequestUrl.toString() + "'");
+		console.log("Fetching and returning data from '" + reverseProxiedRequestUrl.toString() + "'");
 
-			return await fetch(reverseProxiedRequestUrl.toString())
+		let proxiedResponse = await fetch(reverseProxiedRequestUrl.toString())
+
+		if (isMainResumeUrl) {
+			return proxiedResponse;
+		}
+		else {
+			return new HTMLRewriter()
+				.on('nav.css-1si0cz4', new StandardResumeFooterRemover())
+				.transform(proxiedResponse);
+		}
 	},
 };
+
+class StandardResumeFooterRemover implements HTMLRewriterElementContentHandlers {
+	element(element?: Element) {
+		element?.remove();
+	}
+}
