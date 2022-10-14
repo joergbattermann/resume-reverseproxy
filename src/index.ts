d3e608d40b726f7868b1590628263195be0debce
Jörg Battermann <jb@joergbattermann.com>
2022-10-14 19:16:04 -0400

    hrm

--- src/index.ts
+++ src/index.ts
@@ -74,6 +74,7 @@ class StandardResumeFooterRemover {
 	element(element: Element) {
 		if(!(!!!element)){
 			console.log("Removing footer element from StandardResume response")
+			console.log(element)
 			element.setInnerContent('<p>Hello!</p>')
 		}
 		else {
